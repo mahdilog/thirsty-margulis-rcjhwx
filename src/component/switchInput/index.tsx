7e8e0858b18ce8ef1301a8d3d507abfd613e2aa9
CodeSandbox <support@codesandbox.io>
2023-08-27 09:34:15 +0000

    Sandbox snapshot commit

---
+++ src/component/switchInput/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const SwitchInput = ({ formik, name }: any) => {
+  const handleChange = (value) => {
+    formik.setFieldValue(`${name}`, {
+      key: "AMOUNT",
+      value: value.target.value,
+    });
+  };
+
+  const setOption = (value) => {
+    formik.setFieldValue(`${name}`, {
+      key: value,
+      value: formik.values[name].value,
+    });
+  };
+  return (
+    <>
+      <input
+        name={name}
+        onChange={handleChange}
+        value={formik.values[name].value}
+      />
+      <div onClick={() => setOption("PERCENT")}>درصد</div>
+      <div onClick={() => setOption("AMOUNT")}>تومان</div>
+    </>
+  );
+};
+
+export default SwitchInput;
